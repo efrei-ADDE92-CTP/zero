c8c41b88aedd12071ae4dca4a033d51c7d8a2645
Germain-D <germain.deffontaines@efrei.net>
2023-02-08 11:32:29 +0100

    New time metrics

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Request
 import joblib, json
-from prometheus_client import Counter
-from prometheus_client.core import CollectorRegistry
+from prometheus_client import Counter, Summary
 import prometheus_client
-
+import time
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -15,6 +15,18 @@ iris = load_iris()
 X = iris.data 
 y = iris.target
 X_train, X_test, y_train, y_test =train_test_split(X, y, test_size = 0.3, random_state = 2020)
+
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+c = Counter("Classifier_request_count", "Number of requests processed")
+
+
+
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
+
 
 @app.post("/predict-iris")
 async def get_iris(info : Request):
@@ -39,12 +51,12 @@ async def get_iris(info : Request):
 
     return json.dumps(pred_result.tolist())
 
-c = Counter("Classifier_g4_request_count", "Number of requests processed")
 
 @app.get("/metrics")
 def metrics():
     res = []
     res.append(prometheus_client.generate_latest(c))
+    res.append(prometheus_client.generate_latest(REQUEST_TIME))
     return res
 
 #Launch API : uvicorn main:app --reload
